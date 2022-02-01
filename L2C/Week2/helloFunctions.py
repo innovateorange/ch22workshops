4e83ab4d6406f38813d96390a2a9ec329ee522ab
mcufari <mcufari@syr.edu>
2022-02-01 13:28:40 -0500

    more additions to l2c week2

---
+++ L2C/Week2/helloFunctions.py
@@ -0,0 +1,10 @@
+"""
+This is a short program to introduce functions
+"""
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
---
+++ L2C/Week2/learnModules.py
@@ -0,0 +1,13 @@
+"""
+This is a short program to demonstate the power of modules
+"""
+
+from math import sqrt
+import random
+
+if __name__ == "__main__":
+
+    print("The square root of 4 is:",sqrt(4))
+    print("A random value between 0 and 1 is:", random.random())
+
+    
